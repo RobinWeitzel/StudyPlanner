ffd0393c2f111e06a46a4f8974fddc1eee6f49ba
RobinWeitzel <robin.weitzel.rw@gmail.com>
2017-10-29 16:21:30 +0100

    Started adding remoteStorage.

---
+++ customModule.js
@@ -0,0 +1,36 @@
+RemoteStorage.defineModule('studyPlanner', function (privateClient) {
+
+    privateClient.declareType('course', {
+        "type": "object",
+        "properties": {
+            "moduleId": { type: 'string' },
+            "moduleGroup": { type: 'string' },
+            "mark": { type: 'string' },
+            "moduleLp": { type: 'string' },
+            "semester": { type: 'string' },
+            "moduleName": { type: 'string' },
+        },
+        "required": ["moduleId", "moduleGroup", "moduleName", "moduleLp"]
+    });
+
+    // Return and export public functions
+    return {
+        exports: {
+            init: function () {
+                privateClient.cache('');
+            },
+
+            on: privateClient.on,
+
+            addCourse: function (course) {
+                return privateClient.storeObject('course', course.moduleId, course);
+            },
+
+            removeDrink: privateClient.remove.bind(privateClient),
+
+            listCourses: function () {
+                return privateClient.getAll('');
+            }
+        }
+    };
+});
